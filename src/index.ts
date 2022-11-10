8f01fc51c8d5b14854a552d346e4de4be8b6d9e4
Delano van der Waal <d.g.d.van.der.waal@st.hanze.nl>
2022-11-10 17:23:00 +0100

    Added base code structure Biome, Bestagon, Game.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-import {Canvas} from "./helper/canvas";
+import {Canvas} from "./util/Canvas";
+import {Game} from "./game/Game";
+import {GameHelper} from "./game/GameHelper";
 const canvas = new Canvas();
+
+const game = new Game();
 
 main();
 function main() {
@@ -7,5 +11,14 @@ function main() {
     alert("This browser does not support the game!");
     return;
   }
+  GameHelper.generateTinyGrid(game);
+
+  setTimeout(() => {
+    console.log(game.bestagonCount)
+    console.log(game.availableCoordinates)
+    console.log(game.occupiedCoordinates)
+    game.generateBestagon({x: 1, y: 1, z: -2});
+  }, 1000)
+
 
 }
